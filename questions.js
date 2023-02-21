b6b5c99e8ff9776fdca08e867895b169495d6df3
DessySI <divanova.docs.manager@gmail.com>
2023-02-21 15:35:04 +0000

    validation added to questions

--- questions.js
+++ questions.js
@@ -4,22 +4,50 @@ const questions = [
     name: "title",
     message: "What is the title of your project?",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to add title.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "description",
     message:
       "Provide a short description explaining the what, why, and how of your project.",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to add description.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "installation",
     message: "Describe the steps required to install your project? ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to describe the steps required to install your project. If no instalation is required, make a note.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "usage",
     message: "Provide instructions and examples for use. ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to provide instructions and examples for use.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "licenseList",
@@ -46,21 +74,49 @@ const questions = [
     message:
       "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "If you don't like other developers to contribute your application or package, make a note of that.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "tests",
     message: "Write tests for your application. ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to add tests for your application. If no tests required, make a note.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "gitHubUserName",
     message: "Provide gitHub username: ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to add gitHub username. If you don't have any, make a note.";
+      } else {
+        return true;
+      }
+    },
   },
   {
     name: "eMail",
     message: "Provide eMail adderess: ",
     type: "input",
+    validate: (input) => {
+      if (input.trim() === "") {
+        return "You have to add eMail adderess. If you don't have any, make a note.";
+      } else {
+        return true;
+      }
+    },
   },
 ];
 
